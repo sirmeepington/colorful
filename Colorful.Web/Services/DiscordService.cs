dfa390d519c1bca9d802003ffb907c018e69fa3c
Aidan <sir.meepington.1@gmail.com>
2021-06-21 21:34:02 +0100

    Document services, Add validity check to color guilds.

--- Colorful.Web/Services/DiscordService.cs
+++ Colorful.Web/Services/DiscordService.cs
@@ -13,6 +13,8 @@ using System.Threading.Tasks;
 
 namespace Colorful.Web.Services
 {
+
+    /// <inheritdoc cref="IDiscordService"/>
     public class DiscordService : IDiscordService
     {
         private readonly DiscordRestClient _restClient;
@@ -26,6 +28,7 @@ namespace Colorful.Web.Services
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bot", Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN"));
         }
 
+        /// <inheritdoc/>
         public async Task<List<DiscordGuild>> GetGuildsInCommon(ulong userId)
         {
 
@@ -33,21 +36,39 @@ namespace Colorful.Web.Services
             List<DiscordGuild> sharedGuilds = new List<DiscordGuild>();
             foreach (var guild in guilds)
             {
+                if (!IsValid(guild))
+                    continue;
                 var members = await _restClient.ListGuildMembersAsync(guild.Id, null, null);
+
                 if (members.Any(x => x.Id == userId))
-                {
                     sharedGuilds.Add(guild);
-                }
             }
             return sharedGuilds;
         }
 
+        /// <summary>
+        /// Returns whether or not the guild is considered valid.
+        /// <br/>
+        /// Validity ensures that the bot has permissions in the
+        /// server and can manage roles in the server to assign color roles
+        /// via the web interface..
+        /// </summary>
+        /// <param name="guild">The <see cref="DiscordGuild"/> to 
+        /// check the validity of.</param>
+        /// <returns>Whether or not the <see cref="DiscordGuild"/> is valid.</returns>
+        private bool IsValid(DiscordGuild guild)
+        {
+            return guild.Permissions.HasValue && guild.Permissions.Value.HasPermission(Permissions.ManageRoles);
+        }
+
+        /// <inheritdoc/>
         public async Task<DiscordGuild> GetGuild(ulong guildId)
         {
             var guilds = await _restClient.GetCurrentUserGuildsAsync();
             return guilds.FirstOrDefault(x => x.Id == guildId);
         }
 
+        /// <inheritdoc/>
         public async Task<DiscordRole> GetColorRoleFromGuild(ulong user, ulong guild)
         {
             var getResponse = await _httpClient.GetAsync($"https://discord.com/api/v8/guilds/{guild}/members/{user}");
